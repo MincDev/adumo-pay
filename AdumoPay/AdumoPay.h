77cd1cfe873c1f851b7f683140e4c097cd80a107
Christopher Smit <christopher@mincdevelopment.co.za>
2022-12-01 14:34:20 +0200

    Initial Commit

---
+++ AdumoPay/AdumoPay.h
@@ -0,0 +1,18 @@
+//
+//  AdumoPay.h
+//  AdumoPay
+//
+//  Created by Christopher Smit on 2022/12/01.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AdumoPay.
+FOUNDATION_EXPORT double AdumoPayVersionNumber;
+
+//! Project version string for AdumoPay.
+FOUNDATION_EXPORT const unsigned char AdumoPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AdumoPay/PublicHeader.h>
+
+
